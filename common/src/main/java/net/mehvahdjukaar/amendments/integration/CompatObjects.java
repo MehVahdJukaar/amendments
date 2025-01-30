aa226db33fee75acc3a1246d74cfbf03b6855f60
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-01-30 17:02:45 +0100

    candle
    (cherry picked from commit 2a2f4682a6e10daefade651dad4c3f6866910530)

--- common/src/main/java/net/mehvahdjukaar/amendments/integration/CompatObjects.java
+++ common/src/main/java/net/mehvahdjukaar/amendments/integration/CompatObjects.java
@@ -16,12 +16,15 @@ import java.util.function.Supplier;
 public class CompatObjects {
 
     public static final Supplier<Item> SOUL_CANDLE_ITEM = make("buzzier_bees:soul_candle", BuiltInRegistries.ITEM);
+    public static final Supplier<Item> ENDER_CANDLE_ITEM = make("buzzier_bees:ender_candle", BuiltInRegistries.ITEM);
+    public static final Supplier<Item> CUPRIC_CANDLE_ITEM = make("buzzier_bees:cupric_candle", BuiltInRegistries.ITEM);
+    public static final Supplier<Item> SPECTACLE_CANDLE_ITEM = make("cave_enhancements:spectacle_candle", BuiltInRegistries.ITEM);
 
     public static final Supplier<Block> SOUL_CANDLE = make("buzzier_bees:soul_candle", BuiltInRegistries.BLOCK);
     public static final Supplier<Block> ENDER_CANDLE = make("buzzier_bees:ender_candle", BuiltInRegistries.BLOCK);
-    public static final Supplier<Block> CUPRIC_CANDLE = make("caves_and_chasms:cupric_candle", BuiltInRegistries.BLOCK);
+    public static final Supplier<Block> CUPRIC_CANDLE = make("buzzier_bees:cupric_candle", BuiltInRegistries.BLOCK);
+    public static final Supplier<Block> SPECTACLE_CANDLE = make("cave_enhancements:spectacle_candle", BuiltInRegistries.BLOCK);
 
-    public static final Supplier<Item> SPECTACLE_CANDLE_ITEM = make("cave_enhancements:spectacle_candle", BuiltInRegistries.ITEM);
 
     public static final Supplier<Item> ETCHED_DISC = make("etched:etched_music_disc", BuiltInRegistries.ITEM);
 
@@ -31,11 +34,10 @@ public class CompatObjects {
     public static final Supplier<Item> SCONCE_LEVER = make("supplementaries:sconce_lever", BuiltInRegistries.ITEM);
     public static final Supplier<Item> SCONCE = make("supplementaries:sconce", BuiltInRegistries.ITEM);
 
-    public static final Supplier<Block> SPECTACLE_CANDLE = make("cave_enhancements:spectacle_candle", BuiltInRegistries.BLOCK);
 
     public static final Supplier<ParticleType<?>> SMALL_SOUL_FLAME = make("buzzier_bees:small_soul_fire_flame", BuiltInRegistries.PARTICLE_TYPE);
-    public static final Supplier<ParticleType<?>> SMALL_ENDER_FLAME = make("endergetic:small_ender_flame", BuiltInRegistries.PARTICLE_TYPE);
-    public static final Supplier<ParticleType<?>> SMALL_CUPRIC_FLAME = make("caves_and_chasms:small_cupric_flame", BuiltInRegistries.PARTICLE_TYPE);
+    public static final Supplier<ParticleType<?>> SMALL_ENDER_FLAME = make("endergetic:small_ender_fire_flame", BuiltInRegistries.PARTICLE_TYPE);
+    public static final Supplier<ParticleType<?>> SMALL_CUPRIC_FLAME = make("caverns_and_chasms:small_cupric_fire_flame", BuiltInRegistries.PARTICLE_TYPE);
     public static final Supplier<DataComponentType<?>> DISC_APPEARANCE = make("etched:disc_appearance", BuiltInRegistries.DATA_COMPONENT_TYPE);
 
 
