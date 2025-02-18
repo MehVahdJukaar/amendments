fee56307f1807f7e2ebd832e5348639688c23ef0
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-02-18 20:04:08 +0100

    fireball stuff part 2

--- common/src/main/java/net/mehvahdjukaar/amendments/mixins/AbstractHurtingProjectileMixin.java
+++ common/src/main/java/net/mehvahdjukaar/amendments/mixins/AbstractHurtingProjectileMixin.java
@@ -1,5 +1,6 @@
 package net.mehvahdjukaar.amendments.mixins;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.world.entity.Entity;
@@ -28,4 +29,6 @@ public abstract class AbstractHurtingProjectileMixin extends Entity {
 
         return yLocal + this.getBbHeight() / 2.0;
     }
+
+    @WrapOperation(method = "part")
 }
--- common/src/main/java/net/mehvahdjukaar/amendments/mixins/FireballMixin.java
+++ common/src/main/java/net/mehvahdjukaar/amendments/mixins/FireballMixin.java
@@ -1,22 +1,41 @@
 package net.mehvahdjukaar.amendments.mixins;
 
+import net.mehvahdjukaar.amendments.common.entity.ImprovedFireball;
 import net.mehvahdjukaar.amendments.configs.ClientConfigs;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.projectile.Fireball;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 
 // just here because it annoyed me that you can pick these
 @Mixin(Fireball.class)
-public abstract class FireballMixin extends Entity {
+public abstract class FireballMixin extends Entity implements ImprovedFireball {
+
+    @Unique
+    @Nullable
+    private Vec3 amendments$lastParticlePos = null;
 
     public FireballMixin(EntityType<?> entityType, Level level) {
         super(entityType, level);
     }
+
+    @Override
+    public Vec3 amendments$getLasParticlePos() {
+        return this.amendments$lastParticlePos;
+    }
+
+    @Override
+    public void amendments$setLastParticlePos(@NotNull Vec3 pos) {
+        this.amendments$lastParticlePos = pos;
+    }
+
 
     @Shadow public abstract ItemStack getItem();
 
