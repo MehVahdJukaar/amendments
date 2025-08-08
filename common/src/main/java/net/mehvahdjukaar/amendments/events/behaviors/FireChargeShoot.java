8f8acaf8a2e844bc71ecc6db9fed180865ce18e0
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-08-08 19:12:12 +0200

    cooldown
    (cherry picked from commit 2a9c5bd591c93416aed77ce1fba2cff9cf229084)

--- common/src/main/java/net/mehvahdjukaar/amendments/events/behaviors/FireChargeShoot.java
+++ common/src/main/java/net/mehvahdjukaar/amendments/events/behaviors/FireChargeShoot.java
@@ -3,22 +3,19 @@ package net.mehvahdjukaar.amendments.events.behaviors;
 import net.mehvahdjukaar.amendments.common.ProjectileStats;
 import net.mehvahdjukaar.amendments.common.entity.MediumFireball;
 import net.mehvahdjukaar.amendments.configs.CommonConfigs;
-import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.FireChargeItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
 
-public class FireChargeShoot implements ItemUse{
+public class FireChargeShoot implements ItemUse {
 
     @Override
     public boolean isEnabled() {
@@ -35,7 +32,8 @@ public class FireChargeShoot implements ItemUse{
         ItemStack itemStack = player.getItemInHand(usedHand);
         //   this.playSound(level, blockPos);
         //same as in ThrowableProjectile
-        level.playSound(null, player.getX(), player.getEyeY() - 0.1, player.getZ(), SoundEvents.FIRECHARGE_USE, SoundSource.NEUTRAL, 0.5F, 0.6f+ (0.4F / (level.getRandom().nextFloat() * 0.4F + 0.8F)));
+        level.playSound(null, player.getX(), player.getEyeY() - 0.1, player.getZ(), SoundEvents.FIRECHARGE_USE, SoundSource.NEUTRAL, 0.5F, 0.6f + (0.4F / (level.getRandom().nextFloat() * 0.4F + 0.8F)));
+        player.getCooldowns().addCooldown(itemStack.getItem(), 10);
         if (!level.isClientSide) {
             MediumFireball ball = new MediumFireball(level, player);
             ball.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, ProjectileStats.THROWN_SPEED, 1f);
