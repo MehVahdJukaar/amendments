fb7f086e4ec0602c9f9d034de648f2a01f0c1c8f
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-08-15 22:49:14 +0200

    pickup delay

--- common/src/main/java/net/mehvahdjukaar/amendments/common/recipe/CauldronRecipe.java
+++ common/src/main/java/net/mehvahdjukaar/amendments/common/recipe/CauldronRecipe.java
@@ -1,5 +1,6 @@
 package net.mehvahdjukaar.amendments.common.recipe;
 
+import net.mehvahdjukaar.moonlight.api.fluids.SoftFluidStack;
 import net.minecraft.core.NonNullList;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
@@ -8,6 +9,7 @@ import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.ShapelessRecipe;
 
 public class CauldronRecipe extends ShapelessRecipe {
+
     public CauldronRecipe(ResourceLocation id, String group, CraftingBookCategory category, ItemStack result, NonNullList<Ingredient> ingredients) {
         super(id, group, category, result, ingredients);
     }
