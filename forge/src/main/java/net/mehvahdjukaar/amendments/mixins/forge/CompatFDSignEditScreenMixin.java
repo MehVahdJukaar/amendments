35a4c51e9a00c391f2b1e6345b21ac243d1a5013
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-08-22 09:09:38 +0200

    dumb

--- forge/src/main/java/net/mehvahdjukaar/amendments/mixins/forge/CompatFDSignEditScreenMixin.java
+++ forge/src/main/java/net/mehvahdjukaar/amendments/mixins/forge/CompatFDSignEditScreenMixin.java
@@ -1,20 +1,16 @@
 package net.mehvahdjukaar.amendments.mixins.forge;
 
-import com.mojang.math.Axis;
-import net.mehvahdjukaar.amendments.AmendmentsClient;
 import net.mehvahdjukaar.amendments.client.renderers.SignRendererExtension;
 import net.mehvahdjukaar.amendments.configs.ClientConfigs;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.core.Direction;
+import net.minecraft.client.gui.screens.inventory.AbstractSignEditScreen;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.StandingSignBlock;
-import net.minecraft.world.level.block.WallHangingSignBlock;
 import net.minecraft.world.level.block.WallSignBlock;
+import net.minecraft.world.level.block.entity.SignBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -22,11 +18,12 @@ import vectorwing.farmersdelight.client.gui.CanvasSignEditScreen;
 
 @Pseudo
 @Mixin(CanvasSignEditScreen.class)
-public class CompatFDSignEditScreenMixin {
+public abstract class CompatFDSignEditScreenMixin extends AbstractSignEditScreen {
 
-    @Shadow
-    @Final
-    protected boolean isFrontText;
+
+    public CompatFDSignEditScreenMixin(SignBlockEntity sign, boolean isFrontText, boolean isFiltered) {
+        super(sign, isFrontText, isFiltered);
+    }
 
     @Inject(method = "renderSignBackground",
             at = @At("HEAD"), cancellable = true)
@@ -35,7 +32,7 @@ public class CompatFDSignEditScreenMixin {
             Block block = state.getBlock();
             boolean b = block instanceof WallSignBlock;
             if (b || block instanceof StandingSignBlock) {
-                SignRendererExtension.renderSignBlockModelInGui(guiGraphics, b, state, !isFrontText);
+                SignRendererExtension.renderSignBlockModelInGui(guiGraphics, b, state, !this.isFrontText);
                 ci.cancel();
             }
         }
