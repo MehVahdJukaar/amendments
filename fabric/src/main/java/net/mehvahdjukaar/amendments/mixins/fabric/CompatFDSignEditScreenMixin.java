ebce5b4338f111e899ca04163df8be63b54d0918
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-08-22 09:09:38 +0200

    dumb
    (cherry picked from commit 35a4c51e9a00c391f2b1e6345b21ac243d1a5013)

--- fabric/src/main/java/net/mehvahdjukaar/amendments/mixins/fabric/CompatFDSignEditScreenMixin.java
+++ fabric/src/main/java/net/mehvahdjukaar/amendments/mixins/fabric/CompatFDSignEditScreenMixin.java
@@ -8,11 +8,13 @@ import net.mehvahdjukaar.amendments.AmendmentsClient;
 import net.mehvahdjukaar.amendments.client.renderers.SignRendererExtension;
 import net.mehvahdjukaar.amendments.configs.ClientConfigs;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.inventory.AbstractSignEditScreen;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.StandingSignBlock;
 import net.minecraft.world.level.block.WallHangingSignBlock;
 import net.minecraft.world.level.block.WallSignBlock;
+import net.minecraft.world.level.block.entity.SignBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -25,9 +27,11 @@ import vectorwing.farmersdelight.client.gui.CanvasSignEditScreen;
 
 @Pseudo
 @Mixin(CanvasSignEditScreen.class)
-public class CompatFDSignEditScreenMixin {
+public abstract     class CompatFDSignEditScreenMixin extends AbstractSignEditScreen {
 
-    @Shadow @Final protected boolean isFrontText;
+    public CompatFDSignEditScreenMixin(SignBlockEntity sign, boolean isFrontText, boolean isFiltered) {
+        super(sign, isFrontText, isFiltered);
+    }
 
     @Inject(method = "renderSignBackground",
             at = @At("HEAD"), cancellable = true)
@@ -36,7 +40,7 @@ public class CompatFDSignEditScreenMixin {
             Block block = state.getBlock();
             boolean b = block instanceof WallSignBlock;
             if (b || block instanceof StandingSignBlock) {
-                SignRendererExtension.renderSignBlockModelInGui(guiGraphics, b, state, !isFrontText);
+                SignRendererExtension.renderSignBlockModelInGui(guiGraphics, b, state, !this.isFrontText);
                 ci.cancel();
             }
         }
